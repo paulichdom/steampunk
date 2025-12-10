ee156587bd0be0daa2cb6996c0b5b21eaaad7c62
paulichdom <paulic.domagoj@gmail.com>
2025-12-10 22:44:08 +0100

    Refactor Player and Game classes for improved structure and functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   // canvas setup
   const canvas = this.document.getElementById('canvas1');
   const ctx = canvas.getContext('2d');
@@ -19,7 +19,22 @@ window.addEventListener('load', function() {
   }
 
   class Player {
+    constructor(game) {
+      this.game = game;
+      this.width = 120;
+      this.height = 190;
+      this.x = 20;
+      this.y = 100;
+      this.speedY;
+    }
 
+    update() {
+      this.y += this.speedY;
+    }
+
+    draw(context) {
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
   }
 
   class Enemy {
@@ -39,6 +54,20 @@ window.addEventListener('load', function() {
   }
 
   class Game {
-    
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+      this.player = new Player(this);
+    }
+
+    update() {
+      this.player.update()
+    }
+
+    draw(context) {
+      this.player.draw(context);
+    }
   }
+
+  const game = new Game(canvas.width, canvas.height)
 })
